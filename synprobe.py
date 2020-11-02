2ac734e3561d9cb30a6bbd674e5f7cc1b982d936
Minqi Shi <mishi@cs.stonybrook.edu>
2020-11-02 18:35:18 -0500

    I think I am done with part1

--- synprobe.py
+++ synprobe.py
@@ -3,27 +3,51 @@
 
 from scapy.all import *
 import sys
-import socket
-from multiprocessing import Process
-from itertools import count
 import time
 
+
+opened_dict = {}
 
 def scan(target, deport):
     # The following expressions are copyed from scapy reference
     # and the youtube video
+    global opened_dict
     try:
         response = sr1(IP(dst=target)/TCP(dport=deport,flags='S'), timeout=1.14514, verbose=0)
-        if response.haslayer(TCP) and response.getlayer(TCP).flags==0x12:
+        if response!= None and response.haslayer(TCP) and response.getlayer(TCP).flags==0x12:
             # the port is open!
-            print('open')
-            pass
+            opened_dict.setdefault(deport,'open')
+        elif response!= None and response.haslayer(TCP) and response.getlayer(TCP).flags==0x4:
+            opened_dict.setdefault(deport, 'closed')
         else:
-            while True:
-                # just loop it!
-                time.sleep(19.19)
+            opened_dict.setdefault(deport, 'filtered')
     except AttributeError:
-        time.sleep(19.19)
+        pass
+
+
+def my_hexdump(x, indent):
+    """Build a tcpdump like hexadecimal view
+    This function is altered version from the original hexdump in utils.py
+    :param x: a Packet
+    """
+    s = ""
+    x = bytes_encode(x)
+    x_len = len(x)
+    i = 0
+    while i < min(x_len, 1024):
+        if i != 0:
+            s+= indent
+        s += "%04x  " % i
+        for j in range(16):
+            if i + j < x_len:
+                s += "%02X " % orb(x[i + j])
+            else:
+                s += "   "
+        s += " %s\n" % sane_color(x[i:i + 16])
+        i += 16
+    # remove trailing \n
+    s = s[:-1] if s.endswith("\n") else s
+    return s
 
 
 def toList(s):
@@ -46,41 +70,15 @@ def toList(s):
 
 def getFingerPrint(target, deport):
     fingerprint = 'some unique fingerprint'
-    # The following codes are inspired from creating a socket section
-    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # try:
-    #     s.connect((target,deport))
-    # except OSError:
-    #     return ''
-    # s.settimeout(1.14514)
-    # msg = 'I hate this h******k so much; but I am not goind to say it :)'.encode()
+    # The following codes are inspired from youtube
     for i in range(3):
         packet = IP(dst=target)/TCP(dport=deport, flags='S')
-        response = sr1(packet, timeout=1.14514)
+        response = sr1(packet, timeout=1.114514)
         if response != None:
-            return response[0:1024]
+            indent = (4 + 2 + len(str(deport)))*' '
+            return my_hexdump(response, indent)
         else:
             continue
-        # s.sendall(msg)
-        # bytes_recd = 0
-        # chunks = []
-        # to_continue = False
-        # while bytes_recd < 1024:
-        #     chunk = b''
-        #     try:
-        #         chunk = s.recv(min(1024, 1024-bytes_recd))
-        #     except socket.timeout:
-        #         chunk = b''
-        #     print(0)
-        #     if chunk == b'':
-        #         to_continue = True
-        #         break
-        #     chunks.append(chunk)
-        #     bytes_recd += len(chunk)
-        # if to_continue == True:
-        #     continue
-        # fingerprint = b''.join(chunks)
-        # break
     if fingerprint == 'some unique fingerprint':
         fingerprint = 'Port:{}, 3 requests transmitted, 0 bytes received'
         fingerprint = fingerprint.format(deport)
@@ -90,6 +88,7 @@ def getFingerPrint(target, deport):
 def main():
     # print(sys.argv)
     argv = sys.argv
+    global opened_dict
     if len(argv) != 2 and len(argv) != 4:
         exit(1)
     ports = [80,440,441,442,433]
@@ -100,37 +99,18 @@ def main():
             ports = toList(argv[2])
     target = argv[-1]
     conf.verb = 0
-    # s = sr(IP(dst=target)/TCP(dport=ports,flags='S'))
-    opened_dict = {}
     for i in ports:
-        counter = count(0)
-        pname = 'scan port ' + str(i)
-        p = Process(target=scan, name=pname, args=(target,i,))
-        p.start()
-        p.join(timeout=3.16)
-        if p.is_alive():
-            # print('timeout',i)
-            p.terminate()
-            opened_dict.setdefault(i,'closed')
-        else:
-            opened_dict.setdefault(i,'open')
-        # p.terminate()
-        # ans, unans = sr(IP(dst=target)/TCP(dport=i,flags='S'))
-        # print(ans)
-        # if ans == []:
-        #     print(i,'closed')
-        #     continue
-        # Add reference
-        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        scan(target,i)
     print('PORT STATUS FINGERPRINT')
     for i in ports:
-        if opened_dict[i] != 'open':
+        if opened_dict[i] == 'closed':
             print(i,'closed')
-        else:
+        elif opened_dict[i] == 'filtered':
+            print(i, 'filtered')
+        elif opened_dict[i]== 'open':
             result = getFingerPrint(target,i)
             print(i, 'open', result)
         
-
 
 if __name__ == "__main__":
     main()
