d23cfc1ce181d3ff62361ef72f58736a40171720
jasonshi5310 <jasonshi5310@gmail.com>
2020-11-03 20:13:16 -0500

    part 1 done

--- synprobe.py
+++ synprobe.py
@@ -5,7 +5,6 @@ from scapy.all import *
 import sys
 import time
 import ipaddress
-import socket
 
 
 opened_dict = {}
@@ -27,20 +26,20 @@ def scan(target, deport):
             if response is not None and response.haslayer(TCP):
                 # the port is open!
                 if response.getlayer(TCP).flags==0x12:
-                    opened_dict.setdefault(deport,'open')
+                    opened_dict.setdefault(deport,('open',response))
                     break
                 elif response.getlayer(TCP).flags==0x14:
-                    opened_dict.setdefault(deport,'closed')
+                    opened_dict.setdefault(deport,('closed',1))
                     break
             # elif response!= None and response.haslayer(TCP) and response.getlayer(TCP).flags==0x14:
             #     opened_dict.setdefault(deport, 'closed')
             elif response is not None and response.haslayer(ICMP):
                 if (int(response.getlayer(ICMP).type)==3 and int(response.getlayer(ICMP).code) in [1,2,3,9,10,13]):
-                    opened_dict.setdefault(deport, 'filtered')
+                    opened_dict.setdefault(deport, ('filtered',1))
                     break
             # else:
             #     opened_dict.setdefault(deport, 'filtered')
-        opened_dict.setdefault(deport, 'filtered')
+        opened_dict.setdefault(deport, ('filtered',1))
         # print(deport)
     except AttributeError:
         pass
@@ -48,7 +47,7 @@ def scan(target, deport):
         pass
 
 
-def my_hexdump(x, indent):
+def indent_hexdump(x, indent):
     """Build a tcpdump like hexadecimal view
     This is not an original function!!!!
     This function is altered version from the original hexdump in utils.py
@@ -93,25 +92,39 @@ def toList(s):
         return [int(s)]
 
 
-def getFingerPrint(target, deport):
+def getFingerPrint(target, t):
     # This is not an original function!!!
     # The following codes are inspired from youtube video
     # https://www.youtube.com/watch?v=4Y-MR-Hec5Y
-    # and from python docs
-    # add url later!!!!
+    # and stackoverflow
+    # https://stackoverflow.com/questions/4750793/python-scapy-or-the-like-how-can-i-create-an-http-get-request-at-the-packet-leve
     fingerprint = 'some unique fingerprint'
+    deport, res = t
     for i in range(3):
-        # packet = IP(dst=target)/TCP(dport=deport, flags='S')
-        # response = sr1(packet, timeout=1.114514)
-        # url = 'http://' + str(target) + ':' + str(deport)
-        # response = requests.get(url, stream=True,timeout=1.14)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target,deport))
-        if response != None:
-            indent = (4 + 2 + len(str(deport)))*' '
-            return my_hexdump(response.content, indent)
+        if res == None or res[IP].proto!=6:
+            break
+        ack_no = res[TCP].seq+1
+        p = IP(dst=target)/TCP(sport=res[TCP].dport ,dport=deport, ack=ack_no, seq=res[TCP].ack, flags="A")
+        filter_string = ''#'src net '+target+' and tcp src port '+str(deport)
+        send(p,verbose=0)
+        response = sniff(filter=filter_string, timeout=1.114514)
+        for j in range(3):
+            if len(response)!=0:
+                break
+            else:
+                # print('one more try')
+                header = 'GET / HTTP/1.1\r\nHost: '+target+'\r\n\r\n'
+                p = IP(dst=target)/TCP(sport=res[TCP].dport ,dport=deport, ack=ack_no, seq=res[TCP].ack, flags="PA")/header
+                send(p, verbose=0)
+                response=sniff(filter=filter_string,timeout=2.14**(j+1))
+        if len(response)==0:
+            break
         else:
-            continue
+            indent = (4 + 2 + len(str(deport)))*' '
+            fp = b''
+            for i in response:
+                fp += (bytes(i[IP]))
+            return indent_hexdump(fp, indent)
     if fingerprint == 'some unique fingerprint':
         fingerprint = 'Port:{}, 3 requests transmitted, 0 bytes received'
         fingerprint = fingerprint.format(deport)
@@ -138,28 +151,29 @@ def main():
     if target.find('/')!= -1:
         targets = ipaddress.ip_network(target)
         for host in targets.hosts():
+            opened_dict={}
             for j in ports:
                 scan(str(host), j)
             print('PORT STATUS FINGERPRINT FOR:', host)
             for k in ports:
-                if opened_dict[k] == 'closed':
+                if opened_dict[k][0] == 'closed':
                     print(k,'closed')
-                elif opened_dict[k] == 'filtered':
+                elif opened_dict[k][0] == 'filtered':
                     print(k, 'filtered')
-                elif opened_dict[k]== 'open':
-                    result = getFingerPrint(str(host),k)
+                elif opened_dict[k][0]== 'open':
+                    result = getFingerPrint(str(host),(j, opened_dict[k][1]))
                     print(k, 'open', result)
         exit(0)
     for i in ports:
         scan(target,i)
     print('PORT STATUS FINGERPRINT')
     for i in ports:
-        if opened_dict[i] == 'closed':
+        if opened_dict[i][0] == 'closed':
             print(i,'closed')
-        elif opened_dict[i] == 'filtered':
+        elif opened_dict[i][0] == 'filtered':
             print(i, 'filtered')
-        elif opened_dict[i]== 'open':
-            result = getFingerPrint(target,i)
+        elif opened_dict[i][0]== 'open':
+            result = getFingerPrint(target,(i,opened_dict[i][1]))
             print(i, 'open', result)
         
 
